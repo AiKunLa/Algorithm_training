f3f55f37e2c76c6d39d5b81fe49ea048d5dc7445
AiKunLa <3535648608@qq.com>
2025-05-23 22:44:36 +0800

    交换链表节点

--- JavaScript_Learn/LeetCode/代码随想录/2-链表/3-两两交换链表中的节点.js
+++ JavaScript_Learn/LeetCode/代码随想录/2-链表/3-两两交换链表中的节点.js
@@ -9,6 +9,22 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
-    
+
+var swapPairs = function (head) {
+  // 有头节点的情况
+  // 若头节点为空 只有头节点 链表长度为1
+  if (!head || !head.next || !head.next.next) return head;
+  let cur = new ListNode();
+  let result = cur; //
+  cur.next = head;
+  while (cur.next && cur.next.next) {
+    let temp = cur.next;
+    let temp1 = cur.next.next;
+
+    temp.next = temp1.next;
+    temp1.next = temp;
+    cur.next = temp1;
+    cur = temp;
+  }
+  return result.next;
 };
