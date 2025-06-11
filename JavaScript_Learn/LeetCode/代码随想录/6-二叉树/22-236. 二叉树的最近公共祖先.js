42244d3afdf39dd5e9469a9a3fc35f1ace5b96d6
AiKunLa <3535648608@qq.com>
2025-06-11 22:01:27 +0800

    找最近的祖先

---
+++ JavaScript_Learn/LeetCode/代码随想录/6-二叉树/22-236. 二叉树的最近公共祖先.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  // 向上遍历 后续遍历
+  if (!root) return null;
+  if (root === p || root === q) return root;
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  if (!left && right) return right;
+  if (left && !right) return left;
+};
